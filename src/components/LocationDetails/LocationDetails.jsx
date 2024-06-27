ef9b904496da83075d55d6f980d67971b8f2c65c
Shivani <bhadauriashivani777@gmail.com>
2024-06-27 14:15:06 +0530

    Creating the components and initializing them

---
+++ src/components/LocationDetails/LocationDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LocationDetails = () => {
+    return (
+        <h1>LocationDetails</h1>
+    );
+}
+
+export default LocationDetails;
---
+++ src/components/LocationList/LocationList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LocationList = () => {
+    return (
+        <h1>LocationList</h1>
+    );
+}
+
+export default LocationList;
